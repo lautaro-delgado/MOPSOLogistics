863a1918d8cdb2006ac048098917a924df8782b4
lautarodc <lautaro-dc@hotmail.com>
2018-10-17 12:27:21 -0300

    Implementacion clase Funcion

--- MOPSO18/Funcion.h
+++ MOPSO18/Funcion.h
@@ -1,9 +1,35 @@
 #pragma once
+#include <iostream>
+#include <iomanip>
+#include <cctype>
+#include <ctime>
+#include <cstdlib>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#define PI 3.1416
+using namespace std;
+
 class MOPSO;
 class Funcion
 {
-public:
+  public:
 	Funcion();
+	Funcion(int tipo);
 	virtual ~Funcion();
+	vector<float> calcularCosto(vector<float> posicion);
+	int getSizeObj();
+	float getMaxVar(int obj);
+	float getMinVar(int obj);
+	float getMaxVel(int obj);
+	float getMinVel(int obj);
+  private:
+	int tipo_funcion;
+	int numVar;
+	vector<float> Min_var;
+	vector<float> Max_var;
+	vector<float> Min_vel;
+	vector<float> Max_vel;
 };
-
